6bb383c07b7dee73d5dda3c40a3f89aab1c3b429
Artur <artur.h.0427@gmail.com>
2022-11-20 17:29:00 +0100

    Median of two sorted arrays

---
+++ src/hard/median_of_two_sorted_arrays.rs
@@ -0,0 +1,15 @@
+/// Given two sorted arrays nums1 and nums2 of size m and n
+/// respectively, return the median of the two sorted arrays.
+pub fn run(arr1:&[u8], arr2:&[u8]) -> () {
+    println!("{}", run_inner(arr1, arr2));
+}
+fn run_inner(arr1:&[u8], arr2:&[u8]) -> f32 {
+    let mut arr = vec![arr1, arr2].concat();
+    arr.sort();
+    let len = arr.len();
+    if len % 2 == 0 {
+        return (arr[len/2 - 1] as f32 + arr[len/2] as f32) / 2.
+    }else {
+        return arr[len/2] as f32
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,12 @@
 #[path = "./hard/minimum_window_substring.rs"]
 mod min_window_substring;
 
+#[path = "./hard/median_of_two_sorted_arrays.rs"]
+mod median_of_two_sorted_arrays;
+
 /*- Initialize -*/
 fn main() -> () {
     min_window_substring::run("pafbaidawajiddonaoie", "def");
+    median_of_two_sorted_arrays::run(&[1, 2], &[3]);
 }
 
